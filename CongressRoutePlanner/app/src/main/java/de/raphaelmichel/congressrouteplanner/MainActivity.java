dca1f746b9f24b48e12825e0244f56c94cb6b735
Raphael Michel <mail@raphaelmichel.de>
2015-12-14 19:05:22 +0100

    Providing basic location data

--- CongressRoutePlanner/app/src/main/java/de/raphaelmichel/congressrouteplanner/MainActivity.java
+++ CongressRoutePlanner/app/src/main/java/de/raphaelmichel/congressrouteplanner/MainActivity.java
@@ -1,13 +1,95 @@
 package de.raphaelmichel.congressrouteplanner;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager;
+    private WifiReceiver wifiReceiver;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final View pView = LayoutInflater.from(this).inflate(R.layout.progressbar, null);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(pView);
+        getSupportActionBar().getCustomView().setVisibility(View.GONE);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        webView.getSettings().setSupportZoom(false);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onProgressChanged(WebView v, int progress) {
+                if (progress < 100 && pView.getVisibility() == View.GONE) {
+                    pView.setVisibility(View.VISIBLE);
+                }
+                if (progress == 100) {
+                    pView.setVisibility(View.GONE);
+                }
+            }
+
+        });
+
+        webView.loadUrl(BuildConfig.WEB_URL);
+
+        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        wifiReceiver = new WifiReceiver();
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        wifiManager.startScan();
+    }
+
+    class WifiReceiver extends BroadcastReceiver {
+        public void onReceive(Context c, Intent intent) {
+            webView.post(new Runnable() {
+                @Override
+                public void run() {
+                    List<ScanResult> wifiList = wifiManager.getScanResults();
+                    JSONArray ja = new JSONArray();
+                    for(ScanResult result : wifiList) {
+                        JSONObject jo = new JSONObject();
+                        try {
+                            jo.put("bssid", result.BSSID);
+                            jo.put("level", result.level);
+                            ja.put(jo);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    webView.loadUrl("javascript:calculate_position(" + ja.toString() + ");");
+                }
+            });
+        }
     }
 }
