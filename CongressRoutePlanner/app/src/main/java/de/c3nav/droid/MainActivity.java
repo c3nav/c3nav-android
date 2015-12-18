96e5074c928169e8d083b70fcf0577046b513532
Raphael Michel <mail@raphaelmichel.de>
2015-12-18 14:19:25 +0100

    Fix a possible NullPointerException

--- CongressRoutePlanner/app/src/main/java/de/c3nav/droid/MainActivity.java
+++ CongressRoutePlanner/app/src/main/java/de/c3nav/droid/MainActivity.java
@@ -89,7 +89,11 @@ public class MainActivity extends AppCompatActivity {
 
         @JavascriptInterface
         public String getNearbyStations() {
-            return this.nearbyStations.toString();
+            if (this.nearbyStations != null) {
+                return this.nearbyStations.toString();
+            } else {
+                return "[]";
+            }
         }
 
         public void setNearbyStations(JSONArray nearbyStations) {
