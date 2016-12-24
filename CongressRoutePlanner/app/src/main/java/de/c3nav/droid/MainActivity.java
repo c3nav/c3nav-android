b73feab66887a08aadd7ca05bdba487779da6d6f
Raphael Michel <mail@raphaelmichel.de>
2016-12-24 15:39:47 +0100

    Add back button support

--- CongressRoutePlanner/app/src/main/java/de/c3nav/droid/MainActivity.java
+++ CongressRoutePlanner/app/src/main/java/de/c3nav/droid/MainActivity.java
@@ -264,6 +264,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            finish();
+        }
+    }
+
+    @Override
     public void onAttachedToWindow() {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
     }
