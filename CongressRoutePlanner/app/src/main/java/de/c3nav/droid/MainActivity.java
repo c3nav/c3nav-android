b367e90bc301c9f454d3c2ee0247b642e92a96d9
Jenny Danzmayr <mail@masterbase.at>
2018-11-20 12:53:25 +0100

    added launcher theme for a smoother spashscreen

--- CongressRoutePlanner/app/src/main/java/de/c3nav/droid/MainActivity.java
+++ CongressRoutePlanner/app/src/main/java/de/c3nav/droid/MainActivity.java
@@ -83,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
