b5de510354a6109b43a12ba755389884b454e6ce
Jenny Danzmayr <mail@evilscientress.at>
2018-12-23 14:06:27 +0100

    remove videoview of splashscreen from view after splash screen is done

--- CongressRoutePlanner/app/src/main/java/de/c3nav/droid/MainActivity.java
+++ CongressRoutePlanner/app/src/main/java/de/c3nav/droid/MainActivity.java
@@ -53,6 +53,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.view.ViewManager;
 import android.view.WindowManager;
 import android.webkit.CookieManager;
 import android.webkit.HttpAuthHandler;
@@ -595,7 +596,10 @@ public class MainActivity extends AppCompatActivity
     }
 
     protected void unloadSplashVideo() {
-        logoAnimView.setVideoPath("");
+        if (logoAnimView != null) {
+            ((ViewManager) logoAnimView.getParent()).removeView(logoAnimView);
+            logoAnimView = null;
+        }
     }
 
     protected void skipSplash() {
