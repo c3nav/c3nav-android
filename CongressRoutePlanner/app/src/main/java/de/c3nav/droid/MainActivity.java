db535fc6f9dc9ea9d7348a80efe0884482e2f52e
Laura Klünder <laura@codingcatgirl.de>
2023-12-11 19:26:26 +0100

    remove wifiscanrate setting

--- CongressRoutePlanner/app/src/main/java/de/c3nav/droid/MainActivity.java
+++ CongressRoutePlanner/app/src/main/java/de/c3nav/droid/MainActivity.java
@@ -158,7 +158,6 @@ public class MainActivity extends AppCompatActivity
     private boolean settingKeepOnTop = true;
     private boolean settingKeepScreenOn = true;
     private boolean settingUseWifiLocating = true;
-    private Integer settingWifiScanRate = 30;
     private boolean settingDeveloperModeEnabled = false;
     private String settingDeveloperInstanceUrl = "";
     private String settingDeveloperHttpUser = null;
@@ -488,8 +487,6 @@ public class MainActivity extends AppCompatActivity
             }
             MainActivity.this.evaluateJavascript("nearby_stations_available();");
         }
-
-        settingWifiScanRate = Integer.parseInt(sharedPrefs.getString(getString(R.string.wifi_scan_rate_key), "30"));
 
         setWindowFlags();
 
@@ -1064,11 +1061,6 @@ public class MainActivity extends AppCompatActivity
         @JavascriptInterface
         public void currentLocationRequesteFailed() {
             Toast.makeText(MainActivity.this, R.string.current_location_request_failed, Toast.LENGTH_SHORT).show();
-        }
-
-        @JavascriptInterface
-        public int getWifiScanRate() {
-            return Integer.parseInt(sharedPrefs.getString(getString(R.string.wifi_scan_rate_key), "30"));
         }
     }
 
