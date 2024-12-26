b0c7cb54b634f9e4a614885eacd8b059a5cad75f
Jenny Paxian <github@jenny.earth>
2024-12-26 17:34:23 +0100

    Remove unused import

--- CongressRoutePlanner/app/src/main/java/de/c3nav/droid/MainActivity.java
+++ CongressRoutePlanner/app/src/main/java/de/c3nav/droid/MainActivity.java
@@ -94,7 +94,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
